d61a1e8f5ca0f55a1c2ee63af8e456dc59e23caa
yashgangan32 <yashgangan97@gmail.com>
2025-03-08 16:41:10 +0530

    cors fix

--- server/server.js
+++ server/server.js
@@ -10,7 +10,12 @@ const app = express();
 
 // Middlewares
 app.use(express.json());
-app.use(cors());
+const corsOptions = {
+    origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+    optionsSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
 
 // Connect to MongoDB
 connectDB();
